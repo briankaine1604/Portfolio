06fb77c27ba5a886f23bd4e77217bb0b4c75c7ab
Brian <briankaine1604@gmail.com>
2023-06-26 17:11:41 +0100

    26-06-23

--- components/main.jsx
+++ components/main.jsx
@@ -1,7 +1,10 @@
 "use client"
 import React from 'react'
 import Image from 'next/image';
-
+import { BsGear } from "react-icons/bs";
+import { BsPen } from "react-icons/bs";
+import {PiToolbox} from "react-icons/pi"
+import Link from 'next/link';
 
 const MainSection = () => {
   return (
@@ -15,9 +18,9 @@ const MainSection = () => {
            
         /></div>
         <div className='relative justify-center flex md:flex-row items-center w-full h-full flex-col opacity-90'>{/*To adjust the relative, use top-0 or left-0 based on prefernce */}
-        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 mt-5 md:mt-0 z-10'></div>
-        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 sm:mx-5 my-5 z-10'></div>
-        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 mb-5 md:mb-0 z-10'></div>
+        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 mt-5 md:mt-0 z-10 flex items-center justify-center'><Link href="#" className='mainicon z-10 mainicontransition'><div className='mx-auto w-fit'>Projects <div className=' w-fit mx-auto drop-shadow-2xl'><BsGear  /></div></div></Link></div>
+        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 sm:mx-5 my-5 z-10 flex items-center justify-center'><Link href="#" className='mainicon z-10 mainicontransition'><div className='mx-auto w-fit'>Skills <div className=' w-fit mx-auto drop-shadow-2xl'><PiToolbox  /></div></div></Link></div>
+        <div className='bg-black w-[200] md:w-1/5 md:h-1/2 h-1/4 mb-5 md:mb-0 z-10 flex items-center justify-center'><Link href="#" className='mainicon z-10 mainicontransition'><div className='mx-auto w-fit'>Blog <div className=' w-fit mx-auto drop-shadow-2xl'><BsPen  /></div></div></Link></div>
         </div>
     </div>
     
